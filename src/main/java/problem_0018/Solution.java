fb433da2c8c6a29870397776cc433e03efd7291a
VuxDucGiang <giangducx58@gmail.com>
2026-07-18 21:30:12 +0700

    feat: implement solution 0018

---
+++ src/main/java/problem_0018/Solution.java
@@ -0,0 +1,25 @@
+package problem_0018;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = "";
+        for (int i = A.length()-1; i >= 0; i--) {
+            B += A.charAt(i);
+        }
+
+        if (B.equals(A)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+    }
+}
+
